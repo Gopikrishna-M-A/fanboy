702efbc8c3435f2a27bbb92ba7bdeeee471064e8
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-08-16 18:52:53 +0530

    update

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth/next"
 import { NextResponse } from "next/server"
 import { authOptions } from "../auth/[...nextauth]/options"
 import {
+  createOrder,
   getOrderHistory,
 } from "@/services/orderService"
 
