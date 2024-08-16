773ecbc62a5a46e826e7ad65a99829bee34097ef
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-08-16 11:46:29 +0530

    added error page and caching to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,9 @@
 import Landing from "./_components/Landing"
-import { getAllTeams } from "@/services/teamService"
-import { getJerseys } from "@/services/jerseyService"
 
 const page = async () => {
-  const teams = await getAllTeams()
-  const jerseys = await getJerseys()
   return (
     <div className='mt-14'>
-      <Landing teams={teams} jerseys={jerseys}/>
+      <Landing/>
     </div>
   )
 }
