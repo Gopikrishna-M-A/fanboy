3f1a3c29ea8f36c594af0a704e7beab8b95925f5
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2024-08-17 11:09:50 +0530

    added google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,15 +3,17 @@ import "./globals.css"
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
 import { CartProvider } from "../contexts/cart"
-import Providers from "@/contexts/Providers" 
+import Providers from "@/contexts/Providers"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
-import { GoogleAnalytics } from '@next/third-parties/google';
+import Script from "next/script"
 
 export const metadata = {
   title: "Fanboy Jerseys | Where Fan's Meets a Fan",
-  description: "Discover authentic sports jerseys for true fans. Wide selection of teams and players. High-quality",
-  keywords: "sports jerseys, fan merchandise, authentic jerseys, official team gear, jerseys, football, jersey, soccer",
+  description:
+    "Discover authentic sports jerseys for true fans. Wide selection of teams and players. High-quality",
+  keywords:
+    "sports jerseys, fan merchandise, authentic jerseys, official team gear, jerseys, football, jersey, soccer",
   author: "Fanboy Jerseys",
   viewport: "width=device-width, initial-scale=1",
   robots: "index, follow",
@@ -19,27 +21,26 @@ export const metadata = {
   language: "English",
   revisitAfter: "7 days",
   openGraph: {
-    type: 'website',
-    url: 'https://www.fanboyjerseys.store',
+    type: "website",
+    url: "https://www.fanboyjerseys.store",
     title: "Fanboy Jerseys | Where Fan's Meets a Fan",
-    description: 'Discover authentic sports jerseys for true fans. Wide selection of teams and players.',
+    description:
+      "Discover authentic sports jerseys for true fans. Wide selection of teams and players.",
     images: [
       {
-        url: 'https://fanboy-jerseys-images.s3.eu-north-1.amazonaws.com/logo-foot.jpeg',
+        url: "https://fanboy-jerseys-images.s3.eu-north-1.amazonaws.com/logo-foot.jpeg",
         width: 1200,
         height: 630,
-        alt: 'Fanboy Jerseys Collection',
+        alt: "Fanboy Jerseys Collection",
       },
     ],
   },
   social: {
-    instagram: 'https://www.instagram.com/fnbyjrsy',
+    instagram: "https://www.instagram.com/fnbyjrsy",
   },
   manifest: "/manifest.json",
   icons: {
-    icon: [
-      { url: "/icons/logo-16.png", sizes: "16x16", type: "image/png" },
-    ],
+    icon: [{ url: "/icons/logo-16.png", sizes: "16x16", type: "image/png" }],
   },
   other: {
     "msapplication-TileColor": "#22c55e",
@@ -49,18 +50,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body suppressHydrationWarning >
-      <Providers>
-        <AuthProvider>
-          <CartProvider>
-            <Header />
-            {children}
-            <SpeedInsights />
-            <Analytics />
-            <GoogleAnalytics measurementId="G-HRBHMVN9ET" />
-            <Footer />
-          </CartProvider>
-        </AuthProvider>
+      <head>
+        <Script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-HRBHMVN9ET'></Script>
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HRBHMVN9ET');
+          `}
+        </Script>
+      </head>
+      <body suppressHydrationWarning>
+        <Providers>
+          <AuthProvider>
+            <CartProvider>
+              <Header />
+              {children}
+              <SpeedInsights />
+              <Analytics />
+              <Footer />
+            </CartProvider>
+          </AuthProvider>
         </Providers>
       </body>
     </html>
